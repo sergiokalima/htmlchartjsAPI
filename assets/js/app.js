9e79862d3c7edf6f600752718dff9daea2207ce1
Sergio Riquelme <sriquelmepc@gmail.com>
2023-06-30 13:18:02 -0400

    donut

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const data = {
-    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
-    values: [10, 20, 15, 25, 30, 18],
-  };
 
-  const ctx = document.getElementById("myChart");
-  const myChart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: data.labels,
-      datasets: [
-        {
-          label: "# of Month",
-          data: data.values,
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.5)",
+  // PROCESO GET
+  axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      const data = response.data;
+      const labels = data.map(user => user.name)
+      const values = data.map(user => user.id)
 
-            "rgba(54, 162, 235, 0.5)",
-
-            "rgba(255, 206, 86, 0.5)",
-
-            "rgba(75, 192, 192, 0.5)",
-
-            "rgba(153, 102, 255, 0.5)",
-
-            "rgba(255, 159, 64, 0.5)",
+      const ctx = document.getElementById("myChart");
+      const myChart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Users",
+              data: values,
+            },
           ],
-
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-
-            "rgba(54, 162, 235, 1)",
-
-            "rgba(255, 206, 86, 1)",
-
-            "rgba(75, 192, 192, 1)",
-
-            "rgba(153, 102, 255, 1)",
-
-            "rgba(255, 159, 64, 1)",
-          ],
-
-          borderWidth: 1,
         },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
+      });
+    })
+
 });
